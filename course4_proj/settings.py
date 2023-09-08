74e6f0b8e57a912dd3217b33c0d7317bd7739192
Юрій Гудзовський <yurii.hudzovskyi.kb.2022@lpnu.ua>
2023-09-08 10:27:04 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -45,6 +45,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
         'gh',
+        'django_celery_results',
     ]
 
     MIDDLEWARE = [
@@ -161,3 +162,6 @@ class Dev(Configuration):
     SESSION_COOKIE_SAMESITE = "None"
 
     OMDB_KEY = "c0badf7a"
+
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
--- course4_proj/urls.py
+++ course4_proj/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path
 
 import gh.views
+import movies.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", gh.views.index)
+    path("", gh.views.index),
+        path("search/", movies.views.search, name="search"),
+    path(
+        "search-wait/<uuid:result_uuid>/", movies.views.search_wait, name="search_wait"
+    ),
+    path("search-results/", movies.views.search_results, name="search_results")
 ]
