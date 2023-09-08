e8eb296080a8b16178f81d254d063ca6c4867499
Юрій Гудзовський <yurii.hudzovskyi.kb.2022@lpnu.ua>
2023-09-08 08:32:55 +0000

    Finish github library

--- course4_proj/urls.py
+++ course4_proj/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import gh.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", gh.views.index)
 ]
